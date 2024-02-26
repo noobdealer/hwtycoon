869f759a1862ba07e1d8fa6fe4e5832929516a68
noobdealer <148722982+noobdealer@users.noreply.github.com>
2024-02-25 23:31:51 -0500

    Better main menu

---
+++ scripts/autoload/player_company.gd
@@ -0,0 +1,17 @@
+extends Node
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var _PlayerCompany = {
+		"Cash": 0, ## Money available
+		"CEOName": "Steve Jobs", ## Name of CEO
+		"CompanyName": "McDonalds" ## Company name
+	}
+	print("PlayerCompany initialized")
+
+func set_player_stats(key, value):
+	PlayerCompany[key] = value
+	return
+
+func get_player_stats(key):
+	return PlayerCompany[key]
